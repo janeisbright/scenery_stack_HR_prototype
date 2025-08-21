cb83f5211bc118eb97205863a087456ca5ea886d
janeisbright <jane.bright@colorado.edu>
2025-08-21 13:58:40 -0600

    adjusted R eqn alignment

--- src/screen-name/view/SimScreenView.ts
+++ src/screen-name/view/SimScreenView.ts
@@ -418,7 +418,8 @@ export class SimScreenView extends ScreenView {
   this.realRadiusProperty.link((value: number) => {
    
    // this.RText.string = `Radius: R = (T<sub>Sun</sub>/T) <sup>2</sup> (L/L<sub>Sun</sub>)<sup>1/2</sup> <br/> = ${value.toPrecision(5)} R<sub>Sun</sub>`;
-    this.RText.string = `Radius: R =  (L / 4 \u03c0 \u03c3 T <sup>4</sup>)<sup>1/2</sup> <br/> = ${value.toPrecision(5)} R<sub>Sun</sub>`;
+    this.RText.string = `Radius: R =  (L / 4 \u03c0 \u03c3 T <sup>4</sup>)<sup>1/2</sup> 
+    <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; = ${value.toPrecision(5)} R<sub>Sun</sub>`;
   });
 
   this.colorProperty.link(value => {
