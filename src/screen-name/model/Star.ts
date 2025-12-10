032535186745e7e35e28747ecac7b9c5bd8e72e2
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-12-10 11:02:46 -0500

    Add Star class with default temperature

---
+++ src/screen-name/model/Star.ts
@@ -0,0 +1,18 @@
+/**
+ * TODO: Add description
+ *
+ * @author Jane Bright
+ * @author Agustín Vallejo
+ */
+
+export default class Star {
+
+  // Temperature in Kelvin
+  public temperature: number;
+
+  public constructor() {
+
+    this.temperature = 5700;
+
+  }
+}
